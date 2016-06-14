da8eff4e72d43472e68444af0ca89464fbafa1f1
BingYuan <redishtea@hotmail.com>
2016-06-14 21:00:39 +0800

    Seperated the try function

--- zipcrack.py
+++ zipcrack.py
@@ -1,18 +1,26 @@
 import zipfile
 
+def trypass(file,pw):
+	try:
+		print("Trying : " + pw)
+		file.extractall(pwd=pw)
+		return pw
+	except:
+		return
+
 def main():
 	file = zipfile.ZipFile('test.zip')
 	passfile = open('dictionary.txt')
 
 	for pw in passfile.readlines():
 		pw = pw.strip('\n')
-		try:
-			print("Trying : " + pw)
-			file.extractall(pwd=pw)
-			exit(0)
-		except:
-			pass
-	print("Success")
+		match = trypass(file,pw)
+		if match:
+			print("Success")
+		else:
+			print("Failed")
+	file.close()
+
 
 if __name__ == '__main__':
 	main()
